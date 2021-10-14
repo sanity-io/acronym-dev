28adc34e2168996aea3267518d07a391dcc93597
Martin Jacobsen <controloptioncommand@gmail.com>
2021-10-14 13:24:20 +0200

    Pretty list previews for explainer items

--- studio/schemas/term.js
+++ studio/schemas/term.js
@@ -1,36 +1,72 @@
+import React from "react";
+import { Text, Badge } from "@sanity/ui";
+
 export default {
-    name: 'term',
-    type: 'document',
-    fields: [{
-            name: 'term',
-            type: 'string',
-        },
+  name: "term",
+  type: "document",
+  fields: [
+    {
+      name: "term",
+      type: "string",
+    },
+    {
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "term",
+      },
+    },
+    {
+      name: "explainer",
+      type: "array",
+      of: [
         {
-            name: 'slug',
-            type: 'slug',
-            options: {
-                source: 'term'
-            }
+          type: "object",
+          fields: [
+            {
+              name: "letter",
+              type: "string",
+            },
+            {
+              name: "term",
+              type: "string",
+            },
+          ],
+          preview: {
+            select: {
+              letter: "letter",
+              term: "term",
+            },
+            prepare(selection) {
+              const { letter, term } = selection;
+              return {
+                title: `${term}`,
+                media: () => (
+                  <div
+                    style={{
+                      width: 30,
+                      height: 30,
+                      borderRadius: "50%",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      background: "#ced2d9",
+                    }}
+                  >
+                    <Text size={4} weight="semibold" style={{ color: "white" }}>
+                      {letter}
+                    </Text>
+                  </div>
+                ),
+              };
+            },
+          },
         },
-        {
-            name: 'explainer',
-            type: 'array',
-            of: [{
-                type: 'object',
-                fields: [{
-                        name: 'letter',
-                        type: 'string'
-                    },
-                    {
-                        name: 'term',
-                        type: 'string'
-                    }
-                ]
-            }]
-        },
-        {
-            name: 'description',
-            type: 'portableText'
-        }
-    ]
-}
+      ],
+    },
+    {
+      name: "description",
+      type: "portableText",
+    },
+  ],
+};
