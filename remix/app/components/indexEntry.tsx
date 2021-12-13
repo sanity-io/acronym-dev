7f55405097c0ccc72c1385b8b1168dce0d77450f
Knut Melvær <knut.melvaer@gmail.com>
2021-12-13 15:31:44 -0800

    add linkable letters

--- remix/app/components/indexEntry.tsx
+++ remix/app/components/indexEntry.tsx
@@ -1,3 +1,4 @@
+import { Link } from "remix";
 import Acronym from "./acronym";
 
 type IndexEntryProps = {
@@ -8,12 +9,24 @@ type IndexEntryProps = {
 const IndexEntry = ([letter, acronyms]: any) => {
   return (
     <div key={letter} className="mb-6">
-      <h2 className="sticky m-0 text-pink-600 text-9xl">
-        {letter.toLocaleUpperCase()}
+      <h2 id={letter} className="pt-6">
+        <a
+          href={`#${letter}`}
+          className="m-0 text-pink-600 text-9xl no-underline"
+        >
+          {letter.toLocaleUpperCase()}
+        </a>
       </h2>
       <div>
-      {acronyms.length > 0 && <ul>{acronyms.sort((a:any, b: any) => a.term.localeCompare(b.term)).map(Acronym)}</ul>}
+        {acronyms.length > 0 && (
+          <ul>
+            {acronyms
+              .sort((a: any, b: any) => a.term.localeCompare(b.term))
+              .map(Acronym)}
+          </ul>
+        )}
       </div>
+      {/* <Link to="/">top</Link> */}
     </div>
   );
 };
