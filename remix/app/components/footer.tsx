a8e6aa5fbbd7dc8d7ba4f9016c85c9c7cfb11f58
Knut Melvær <knut.melvaer@gmail.com>
2021-12-13 14:22:33 -0800

    Add link to API

--- remix/app/components/footer.tsx
+++ remix/app/components/footer.tsx
@@ -1,22 +1,48 @@
 import { HeartIcon } from "@sanity/icons";
 import { SanityLogo } from "@sanity/logos";
+import { Link, useParams } from "remix";
 
 export default function Footer() {
+  const { term } = useParams();
+
+  const endpoint = `/resources/api`;
+  const params = {
+    q: `*[_type=='term' && slug.current == "${term}"]`,
+  };
+  const searchString = new URLSearchParams(params).toString();
+  let queryUrl = endpoint + "?" + searchString;
   return (
-    <footer className="flex justify-center w-full bottom-5">
-      <p className="flex justify-center w-1/2 mx-auto prose text-center align-middle">
-        <span className="block">
-        Made with ♥️ by
-        </span>
+    <footer className="flex flex-col md:flex-row justify-center w-full bottom-5">
+      <div className="mb-6 md:mb-0">
+        Want to use the{" "}
+        <Link to="/api" className="bg-pink-600 text-white">
+          API
+        </Link>{" "}
+        instead? Send a{" "}
+        <Link to="/groq" className="bg-pink-600 text-white">
+          GROQ
+        </Link>{" "}
+        query to{" "}
+        <pre className="inline hover:bg-pink-600 hover:text-white">
+          <a href={queryUrl}>/resources/api?q=</a>
+        </pre>
+        .
+      </div>
+      <div className="flex justify-end md:w-1/2 mx-auto prose">
+        <span className="block">Made with ♥️ by</span>
         <a
           href="https://sanity.io"
           target="_blank"
           rel="noopener noreferrer"
           className="display-inline"
         >
-          <SanityLogo aria-label="Sanity" className="inline text-3xl" style={{marginTop: -2}} />
+          <SanityLogo
+            aria-label="Sanity"
+            className="inline text-3xl"
+            style={{ marginTop: -2 }}
+          />
         </a>
-      </p>
+      </div>
     </footer>
   );
 }
