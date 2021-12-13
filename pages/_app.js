1e30215051d4dc369bd934ed3b3d70fd55660ca8
Knut Melvær <knut.melvaer@gmail.com>
2021-12-12 17:47:29 -0800

    remove next app

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import 'tailwindcss/tailwind.css'
-import Footer from '../components/Footer'
-
-function MyApp({ Component, pageProps }) {
-  return <div><Component {...pageProps} /><Footer /></div>
-}
-
-export default MyApp
